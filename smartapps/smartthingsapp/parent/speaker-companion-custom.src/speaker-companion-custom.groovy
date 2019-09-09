f673c7b63208610664c2f0a1488866eca0411bd4
Jonathan Fields <liljonnyfields@gmail.com>
2019-09-09 16:38:02 -0400

    new

---
+++ smartapps/smartthingsapp/parent/speaker-companion-custom.src/speaker-companion-custom.groovy
@@ -0,0 +1,40 @@
+definition(
+    name: "Speaker Companion (Custom)",
+    namespace: "smartThingsAPP/parent",
+    author: "fieldsjm",
+    description: "Control your speaker, play a sound, a custom message when an event occurs or the system mode changes.",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Partner/sonos.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Partner/sonos@2x.png")
+
+
+preferences {
+    // The parent app preferences are pretty simple: just use the app input for the child app.
+    page(name: "mainPage", title: "Speaker Companion", install: true, uninstall: true,submitOnChange: true) {
+        section {
+            app(name: "speakerControl", appName: "Speaker Control", namespace: "smartThingsAPP", title: "Control - play/stop/pause/skip", multiple: true)
+            app(name: "speakerNotify", appName: "Speaker Notify with Sound", namespace: "smartThingsAPP", title: "Play a notification", multiple: true)
+            app(name: "weatherForecast", appName: "Speaker Weather Forecast", namespace: "smartThingsAPP", title: "Play weather report")
+            }
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    // nothing needed here, since the child apps will handle preferences/subscriptions
+    // this just logs some messages for demo/information purposes
+    log.debug "there are ${childApps.size()} child smartapps"
+    childApps.each {child ->
+        log.debug "child app: ${child.label}"
+    }
+}
